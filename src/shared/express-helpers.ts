e37aa02253b77b1cc35fd795d807730277ad3c6d
Amit Mohanty <amit.mohanty@yavda.in>
2026-04-02 21:14:36 +0530

    cleanup: retain only azure/functions/copy-worker; remove other components

--- src/shared/express-helpers.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-export function asyncRoute(
-  handler: (req: express.Request, res: express.Response, next: express.NextFunction) => Promise<void>
-): express.RequestHandler {
-  return (req, res, next) => {
-    void handler(req, res, next).catch(next);
-  };
-}
